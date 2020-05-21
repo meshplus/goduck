bba0930994c5fc442a0adb77f545397a839d8f36
Alexader <tyxj58@gmail.com>
2020-05-21 16:48:13 +0800

    fix(cmd): goduck start command error when not init

--- cmd/goduck/fabric/start.go
+++ cmd/goduck/fabric/start.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/meshplus/goduck/repo"
 	"github.com/urfave/cli/v2"
 )
 
@@ -36,14 +37,22 @@ func GetFabricCMD() *cli.Command {
 }
 
 func start(ctx *cli.Context) error {
-	repoRoot := ctx.String("repo")
+	repoRoot, err := repo.PathRootWithDefault(ctx.String("repo"))
+	if err != nil {
+		return err
+	}
+
 	args := []string{filepath.Join(repoRoot, "ffn.sh"), "up"}
 
 	return execCmd(args)
 }
 
 func installChaincode(ctx *cli.Context) error {
-	repoRoot := ctx.String("repo")
+	repoRoot, err := repo.PathRootWithDefault(ctx.String("repo"))
+	if err != nil {
+		return err
+	}
+
 	fabricConfig := ctx.String("config")
 	args := make([]string, 0)
 	args = append(args, filepath.Join(repoRoot, "chaincode.sh"), "install", "-c", fabricConfig)
