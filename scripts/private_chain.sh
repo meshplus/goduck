4a003bb217e36a59241068d4d8e12342ec3760a8
Alexader <tyxj58@gmail.com>
2020-06-10 16:56:13 +0800

    feat(ether): enable ethereum geth node with dev mode
    1. start ethereum private chain with POA instead of POW
    2. enable private chain with default broker and transfer contract
    3. enable stopping ethereum private chain

--- scripts/private_chain.sh
+++ scripts/private_chain.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+WORKDIR=ethereum
+
 function print_blue() {
   printf "${BLUE}%s${NC}\n" "$1"
 }
@@ -12,38 +14,52 @@ function printHelp() {
   echo "    <mode> - one of 'binary', 'docker'"
   echo "      - 'binary' - bring up the ethereum with local binary geth"
   echo "      - 'docker' - clear the ethereum with geth in docker"
+  echo "      - 'down' - shut down ethereum binary and docker container"
   echo "  private_chain.sh -h (print this message)"
 }
 
 function binaryUp() {
   # clean up datadir
+  cd ${WORKDIR}
   rm -rf datadir
-  mkdir datadir
+  tar xvf datadir.tar.gz
 
-  # init genesis block
-  geth init --datadir=datadir genesis.json
-  cp account.key ./datadir/keystore/account.key
-
-  nohup geth --datadir $HOME/.goduck/datadir --ws --rpc \
+  nohup geth --datadir $HOME/.goduck/ethereum/datadir --dev --ws --rpc \
       --rpccorsdomain https://remix.ethereum.org \
       --wsaddr "0.0.0.0" --rpcaddr "0.0.0.0" --rpcport 8545 \
-      --rpcapi "eth,web3,personal,net,miner,admin,debug" \
-      --allow-insecure-unlock --nodiscover \
-      --unlock 0c7cd0feddf37a350530446bf3ebdddd447d2790 --password password \
-      --mine --miner.threads=1 --etherbase=0c7cd0feddf37a350530446bf3ebdddd447d2790 > /dev/null 2>&1 &
+      --rpcapi "eth,web3,personal,net,miner,admin,debug" >/dev/null 2>&1 &
+  echo $! >ethereum.pid
 }
 
 function dockerUp() {
   docker run -d --name ethereum-node \
   -p 8545:8545 -p 8546:8546 -p 30303:30303 \
   meshplus/ethereum \
-      --datadir /root/datadir --ws --rpc \
-      --rpccorsdomain https://remix.ethereum.org --rpcaddr "0.0.0.0" --rpcport 8545 --wsaddr "0.0.0.0" \
-      --rpcapi "eth,web3,personal,net,miner,admin,debug" \
-      --allow-insecure-unlock --nodiscover \
-      --unlock 0c7cd0feddf37a350530446bf3ebdddd447d2790 --password /root/password \
-      --preload init.js --nousb \
-      --mine --miner.threads=1 --etherbase=0c7cd0feddf37a350530446bf3ebdddd447d2790
+      --datadir /root/datadir --dev --ws --rpc \
+      --rpccorsdomain https://remix.ethereum.org \
+      --rpcaddr "0.0.0.0" --rpcport 8545 --wsaddr "0.0.0.0" \
+      --rpcapi "eth,web3,personal,net,miner,admin,debug"
+}
+
+function etherDown() {
+  set +e
+  if [ -a "${WORKDIR}"/ethereum.pid ]; then
+    list=$(cat "${WORKDIR}"/ethereum.pid)
+    for pid in $list; do
+      kill "$pid"
+      if [ $? -eq 0 ]; then
+        echo "node pid:$pid exit"
+      else
+        print_red "program exit fail, try use kill -9 $pid"
+      fi
+    done
+    rm "${WORKDIR}"/ethereum.pid
+  fi
+
+  if [ "$(docker container ls | grep -c ethereum-node)" -ge 1 ]; then
+    docker stop ethereum-node
+    echo "ethereum docker stop"
+  fi
 }
 
 MODE=$1
@@ -52,6 +68,8 @@ if [ "$MODE" == "binary" ]; then
   binaryUp
 elif [ "$MODE" == "docker" ]; then
   dockerUp
+elif [ "$MODE" == "down" ]; then
+  etherDown
 else
   printHelp
   exit 1
