d3ab6f2bd5ed4deb6720c8cdcff35f5f61f8005b
dawn-to-dusk <1927529251@qq.com>
2021-04-01 17:01:14 +0800

    fix(*): fix a bug about extracting pier binary

--- cmd/goduck/pier.go
+++ cmd/goduck/pier.go
@@ -434,7 +434,7 @@ func downloadPierBinary(repoPath string, version string) error {
 				return err
 			}
 
-			err = sh.Command("/bin/bash", "-c", fmt.Sprintf("cd %s && tar xf pier_linux-amd64_%s.tar.gz -C %s --strip-components 1 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s", root, version, root, root)).Run()
+			err = sh.Command("/bin/bash", "-c", fmt.Sprintf("cd %s && tar xf pier_linux-amd64_%s.tar.gz -C %s --strip-components 2 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s", root, version, root, root)).Run()
 			if err != nil {
 				return fmt.Errorf("extract pier binary: %s", err)
 			}
@@ -455,7 +455,7 @@ func downloadPierBinary(repoPath string, version string) error {
 				return err
 			}
 
-			err = sh.Command("/bin/bash", "-c", fmt.Sprintf("cd %s && tar xf pier_darwin_x86_64_%s.tar.gz -C %s --strip-components 1 && install_name_tool -change @rpath/libwasmer.dylib %s/libwasmer.dylib %s/pier", root, version, root, root, root)).Run()
+			err = sh.Command("/bin/bash", "-c", fmt.Sprintf("cd %s && tar xf pier_darwin_x86_64_%s.tar.gz -C %s --strip-components 2 && install_name_tool -change @rpath/libwasmer.dylib %s/libwasmer.dylib %s/pier", root, version, root, root, root)).Run()
 			if err != nil {
 				return fmt.Errorf("extract pier binary: %s", err)
 			}
