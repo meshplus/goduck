28d177d66048622eab52317bc6048ab79bfbe8de
dawn-to-dusk <1927529251@qq.com>
2021-05-18 16:59:58 +0800

    feat(*): modify how the BitxHub configuration is generated
    Add a script to generate the BitxHub configuration to make generating the BitxHub configuration easier.

--- internal/types/types.go
+++ internal/types/types.go
@@ -12,6 +12,8 @@ const (
 	ChainTypeEther  = "ethereum"
 	ChainTypeFabric = "fabric"
 
+	BxhConfigRepo    = "bxh_config"
+	BitxhubConfig    = "bitxhub_config.sh"
 	PlaygroundScript = "playground.sh"
 	FabricScript     = "fabric.sh"
 	ChaincodeScript  = "chaincode.sh"
@@ -24,6 +26,7 @@ const (
 	TmpPath          = "tmp"
 
 	Pier         = "pier"
+	BitXHub      = "bitxhub"
 	EthClient    = "eth-client"
 	FabricClient = "fabric-client-1.4"
 	RuleName     = "validating.wasm"
@@ -43,5 +46,7 @@ const (
 	FabricRuleUrl   = "https://raw.githubusercontent.com/meshplus/pier-client-fabric/master/config/validating.wasm"
 	EthereumRuleUrl = "https://raw.githubusercontent.com/meshplus/pier-client-ethereum/master/config/validating.wasm"
 
-	FabricConfig = "config.yaml"
+	FabricConfig    = "config.yaml"
+	BxhModifyConfig = "bxh_config.toml"
+	BitxhubTarName  = "bitxhub_%s_x86_64_%s.tar.gz"
 )
