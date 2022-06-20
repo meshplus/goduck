870f3b5f1b48a7bec94e1edd36c46837221dfaa7
weiyijie <hzwyjwork@163.com>
2022-06-20 14:41:57 +0800

    perf:goduck快速开始适配v1.23.0

--- scripts/deploy_contracts.sh
+++ scripts/deploy_contracts.sh
@@ -12,30 +12,31 @@ ACCOUNT_PATH="${CURRENT_PATH}/docker/quick_start"
 CONTRACT_PATH="${CURRENT_PATH}/example"
 
 function deploy_contracts() {
-   print_blue "Deploy broker contract1"
+   print_blue "Deploy Contract in ethereum1"
+   print_blue "Deploy broker contract"
    goduck ether contract deploy --address $ETH_ADDR1 --key-path "$ACCOUNT_PATH"/account.key  --code-path "$CONTRACT_PATH"/broker.sol "1356^ethappchain1^["0xc7F999b83Af6DF9e67d0a37Ee7e900bF38b3D013","0x79a1215469FaB6f9c63c1816b45183AD3624bE34","0x97c8B516D19edBf575D72a172Af7F418BE498C37","0xc0Ff2e0b3189132D815b8eb325bE17285AC898f8"]^1^["0x20F7Fac801C5Fc3f7E20cFbADaA1CDb33d818Fa3"]^1" >"$CONTRACT_PATH/brokerAddr"
    broker_address=$(grep Deployed <"$CONTRACT_PATH/brokerAddr" | grep -o '0x.\{40\}')
-   print_green "broker contract address1: $broker_address"
+   print_green "broker contract address: $broker_address"
 
-   print_blue "Deploy transfer contract1"
+   print_blue "Deploy transfer contract"
    goduck ether contract deploy --address $ETH_ADDR1 --key-path "$ACCOUNT_PATH"/account.key  --code-path "$CONTRACT_PATH"/transfer.sol "$broker_address" >"$CONTRACT_PATH/transferAddr"
    transfer_address=$(grep Deployed <"$CONTRACT_PATH/transferAddr" | grep -o '0x.\{40\}')
-   print_green "transfer contract address1: $transfer_address"
+   print_green "transfer contract address: $transfer_address"
 
    print_blue "aduit contract"
    goduck ether contract invoke --key-path "$ACCOUNT_PATH"/account.key --abi-path "$CONTRACT_PATH"/broker.abi --address $ETH_ADDR1 $broker_address audit "$transfer_address^1"
    print_green "aduit contract aduit:successful"
 
-
-   print_blue "Deploy broker contract2"
+   print_blue "Deploy Contract in ethereum2"
+   print_blue "Deploy broker contract"
    goduck ether contract deploy --address $ETH_ADDR2 --key-path "$ACCOUNT_PATH"/account.key  --code-path "$CONTRACT_PATH"/broker.sol "1356^ethappchain2^["0xc7F999b83Af6DF9e67d0a37Ee7e900bF38b3D013","0x79a1215469FaB6f9c63c1816b45183AD3624bE34","0x97c8B516D19edBf575D72a172Af7F418BE498C37","0xc0Ff2e0b3189132D815b8eb325bE17285AC898f8"]^1^["0x20F7Fac801C5Fc3f7E20cFbADaA1CDb33d818Fa3"]^1" >"$CONTRACT_PATH/brokerAddr2"
    broker_address2=$(grep Deployed <"$CONTRACT_PATH/brokerAddr2" | grep -o '0x.\{40\}')
-   print_green "broker contract address2: $broker_address2"
+   print_green "broker contract address: $broker_address2"
 
-   print_blue "Deploy transfer contract2"
+   print_blue "Deploy transfer contract"
    goduck ether contract deploy --address $ETH_ADDR2 --key-path "$ACCOUNT_PATH"/account.key  --code-path "$CONTRACT_PATH"/transfer.sol "$broker_address" >"$CONTRACT_PATH/transferAddr2"
    transfer_address2=$(grep Deployed <"$CONTRACT_PATH/transferAddr2" | grep -o '0x.\{40\}')
-   print_green "transfer contract address2: $transfer_address2"
+   print_green "transfer contract address: $transfer_address2"
 
    print_blue "aduit contract"
    goduck ether contract invoke --key-path "$ACCOUNT_PATH"/account.key --abi-path "$CONTRACT_PATH"/broker.abi --address $ETH_ADDR2 $broker_address2 audit "$transfer_address2^1"
